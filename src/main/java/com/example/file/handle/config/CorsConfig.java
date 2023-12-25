aacc1b9f4a672b7218bae09de7c4ec143ad6be97
Tines Kumar <tineskumar007@gmail.com>
2023-12-26 01:18:09 +0530

    CorsConfig added
    CorsConfig added handle network calls from all origins

---
+++ src/main/java/com/example/file/handle/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.example.file.handle.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Allow all origins, methods, and headers. This is just an example; you may want to restrict it in a production environment.
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
